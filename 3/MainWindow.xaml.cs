4fe3e73ca6279a4db38112cba9d18797299b1a7a
purwic <rodion.kochergin@gmail.com>
2023-11-27 13:08:37 +0300

    2 3 4 там

---
+++ 3/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void compute()
+        {
+            try
+            {
+
+                int a = int.Parse(A.Text);
+                int b = int.Parse(B.Text);
+
+                if (a < b)
+                {
+
+                    ulong product = 1;
+
+                    for (int i = a; i <= b; i++)
+                    {
+                        product *= ulong.Parse($"{i}");
+
+                    }
+
+                    if ( a > 0 && product == 0) { throw new ArgumentException(); }
+
+
+
+                    Result.Content = $"{product}";
+
+                }
+                else { throw new ArgumentException(); }
+
+            }
+
+            catch
+            {
+                Result.Content = "-";
+            }
+        }
+
+
+
+        private void A_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            compute();
+        }
+
+        private void B_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            compute();
+        }
+    }
+}
---
+++ 4/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _4
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private ulong factorial (ulong n)
+        {
+
+            if (n == 1) return 1;
+
+            return n * factorial(n - 1);
+
+        }
+
+
+        private void compute()
+        {
+            try
+            {
+
+                ulong n = ulong.Parse(N.Text);
+
+
+
+                if (n > 0)
+                {
+
+                    double sum = 1;
+
+                    for (ulong i = 1; i <= n; i++)
+                    {
+
+                        sum += 1.0 / factorial(i);
+
+                    }
+
+                    // когда превышено значение допустимое ulong, компьютер приравнивает product нулю
+                    if (sum == 0) { throw new ArgumentException(); }
+
+
+
+                    Result.Content = $"{sum}";
+
+                }
+                else { throw new ArgumentException(); }
+
+            }
+
+            catch
+            {
+                Result.Content = "-";
+            }
+        }
+
+
+
+        private void N_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            compute();
+        }
+    }
+}
